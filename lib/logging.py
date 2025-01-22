bb17ec31cefcd6e2cdae413b684c95b379d6b8b6
Ezekiel (Zeke) Hall <hall.ei.2024@wvwc.edu>
2025-01-22 03:02:53 -0500

    update to logging

--- lib/logging.py
+++ lib/logging.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
-def log(text: str) -> None:
+def log(text: str, console: bool = True) -> None:
+    """
+    param text: Takes in a string to log to a file and console
+    """
     logMsg = str(datetime.now().replace(microsecond=0)) + "  ::  " + text + "\n"
     try:
         with open("log.txt", "a", encoding="utf-8") as logFile:
             logFile.write(logMsg)
     except Exception as e:
         print(f"Failed to save to file log. Bypassing file logging. ERROR: {e}")
-    print(logMsg,end="")
+    if console:
+        print(logMsg,end="")
